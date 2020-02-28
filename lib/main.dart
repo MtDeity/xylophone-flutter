fa3d63b0bcb441445f8e2c54d0dadf8804ae50c5
MtDeity <o8olo853q43@gmail.com>
2020-02-28 18:33:29 +0900

    play sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -10,7 +11,13 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: Text(all.first),
+            child: FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+              child: Text('Click Me'),
+            ),
           ),
         ),
       ),
